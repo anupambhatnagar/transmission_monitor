0f3f8abaec4968102e10633173f0fde011ca1315
Anupam Bhatnagar <anupamb@fb.com>
2022-08-12 16:52:17 -0700

    clean up setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import find_packages, setup
-from package import Package
 
 setup(
     name="transmission-monitor",
@@ -13,18 +12,7 @@ setup(
     install_requires=[
         "tk"
     ],
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-    ],
+    classifiers=[ ],
     include_package_data=True,
-    package_data={"": ["configs/*.json", "configs/*.config"]},
+    package_data={"": []},
 )
